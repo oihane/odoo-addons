a12e6cd9f2c67268546c1229383f1742bbb8213d
mikelarre <mikelarregi@avanzosc.es>
2016-05-02 15:29:38 +0200

    [Mod] pep8

--- stock_orderpoint_rule/wizard/orderpoint_procurement.py
+++ stock_orderpoint_rule/wizard/orderpoint_procurement.py
@@ -2,7 +2,7 @@
 # © 2016 Mikel Arregi Etxaniz - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields, api
+from openerp import models, api
 
 
 class ProcurementCompute(models.TransientModel):
